44da5605a2e3221d3801a27a3d3f37ccb171d713
NicoMore <nicomorelli47@gmail.com>
2023-05-17 18:54:23 -0300

    Agregada funcion de mutacion conceptualmente

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygad as pg
 
 # Por ahora copypaste de lo que dice PyGad
+# Posiblemente haya que meterlo en un __init__
 num_generations = 50
 num_parents_mating = 4
 
@@ -23,7 +24,7 @@ mutation_percent_genes = 10
 # Otro burdo copypaste
 ga_instance = pg.GA(num_generations=num_generations,
                        num_parents_mating=num_parents_mating,
-                       fitness_func=fitness_function,
+                       fitness_func=fitness_func,
                        sol_per_pop=sol_per_pop,
                        num_genes=num_genes,
                        init_range_low=init_range_low,
@@ -31,7 +32,7 @@ ga_instance = pg.GA(num_generations=num_generations,
                        parent_selection_type=parent_selection_type,
                        keep_parents=keep_parents,
                        crossover_type=crossover_type,
-                       mutation_type=mutation_type,
+                       mutation_type= mutation_func, # Aca va la func de mutacion de abajo, sino igualar a mutation_type 
                        mutation_percent_genes=mutation_percent_genes)
 
 # Donde solution es el plato, requiere ese nombre x Pygad
@@ -40,3 +41,14 @@ def fitness_func(ga_instance, solution, solution_idx):
     # Pasar numero binario a preferencia
     # Definir func matematica
     return fitness
+
+# No es requerido, pero si definimos la mutacion nosotros podemos trabajar con los platos que definimos, con un random
+# Lo que no se es que pasa si la mutacion resulta ser peor, si la descarta o que hace Pygad por atras
+# Otra opcion es definir los platos como tipos y limitar al gen a esos tipos, tendria que seguir leyendo la docu para ver que onda
+def mutation_func(offspring, ga_instance):
+    # Seleccionar random un plato principal
+    # Seleccionar random un acompañamiento
+    # Seleccionar random una bebida
+    return offspring
+
+# Tambien se pueden definir padres y crossover
