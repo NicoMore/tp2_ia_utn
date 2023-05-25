5f6ee84ab01f189fba3f871342bf2e511c9135d9
NicoMore <nicomorelli47@gmail.com>
2023-05-25 15:32:31 -0300

    FUNCA

--- main.py
+++ main.py
@@ -1,70 +1,100 @@
 import pygad as pg
-import platos
+from platos import *
 
-# Por ahora copypaste de lo que dice PyGad
-# Posiblemente haya que meterlo en un __init__
-num_generations = 50
-num_parents_mating = 4
+def switch_calos(calorias) -> int:
 
-fitness_function = fitness_func
+    if calorias == [1,1]:
+        return 0
+    else:
+        if calorias == [1,0]:
+            return 34
+        else:
+             if calorias == [0,1]:
+                return 67
+             else:
+                 if calorias == [0,0]:
+                     return 100
 
-sol_per_pop = 8 # No hace falta si initial_population
-num_genes = len(function_inputs) # No hace falta si initial_population
+    return -100
+def caloria_calculator(principal, acomp, bebida) -> int:
+    calos_principal = principal[:1]
+    calos_acomp = acomp[:1]
+    calos_bebida = bebida[:1]
 
-init_range_low = -2 # No hace falta si initial_population
-init_range_high = 5 # No hace falta si initial_population
+    return round((switch_calos(calos_principal) + switch_calos(calos_acomp) + switch_calos(calos_bebida)) / 3) # Tope de 100
 
-parent_selection_type = "sss"
-keep_parents = 1
+def gusto_calculator(principal, acomp, bebida) -> int:
+    gusto_principal = principal[2:]
+    gusto_acomp = acomp[2:]
+    gusto_bebida = bebida[2:]
 
-crossover_type = "single_point"
+    gusto_p = int("".join(str(i) for i in gusto_principal), 2)
+    gusto_a = int("".join(str(i) for i in gusto_acomp), 2)
+    gusto_b = int("".join(str(i) for i in gusto_bebida), 2)
 
-mutation_type = "random"
-mutation_percent_genes = 10
-
-# Definido por mi (Nico)
-
-# No esta claro si es superior definirla o no, pero tal vez es interesante ver una especie de "progreso" a la solucion
-initial_population = [
-                    [milanesa, papas_fritas, agua],
-                    [ensalada, arroz, bebida_cola],
-                    [bife, pan, jugo]
-                    ]
-
-# Dice que aguanta listas, no se si listas de listas. Sublista representa gen
-gene_type = [[int, int, int, int, int, int, int], [int, int, int, int], [int, int, int]]
-
-# Limita los genes a los que definimos adentro, idem comentario arriba
-gene_space = [[milanesa, ensalada, bife, pechuga_de_pollo, hamburguesa], [pure_de_papa, ensalada_side, papas_fritas, arroz, pan], [agua, bebida_cola, bebida_cola_sin_azucar, jugo, bebida_energetica]]
-
-# Otro burdo copypaste
-ga_instance = pg.GA(num_generations=num_generations,
-                       num_parents_mating=num_parents_mating,
-                       fitness_func=fitness_func,
-                       sol_per_pop=sol_per_pop,
-                       num_genes=num_genes,
-                       init_range_low=init_range_low,
-                       init_range_high=init_range_high,
-                       parent_selection_type=parent_selection_type,
-                       keep_parents=keep_parents,
-                       crossover_type=crossover_type,
-                       mutation_type= mutation_func, # Aca va la func de mutacion de abajo, sino igualar a mutation_type 
-                       mutation_percent_genes=mutation_percent_genes)
+    return round((gusto_p + gusto_a + gusto_b) * 3 * 100 / 111)
 
 # Donde solution es el plato, requiere ese nombre x Pygad
 def fitness_func(ga_instance, solution, solution_idx):
-    # Chequear vs preferencia calorica
-    # Pasar numero binario a preferencia
-    # Definir func matematica
-    return fitness
+    principal = int(solution[0])
+    acomp = int(solution[1])
+    bebida = int(solution[2])
 
-# No es requerido, pero si definimos la mutacion nosotros podemos trabajar con los platos que definimos, con un random
-# Lo que no se es que pasa si la mutacion resulta ser peor, si la descarta o que hace Pygad por atras
-# Otra opcion es definir los platos como tipos y limitar al gen a esos tipos, tendria que seguir leyendo la docu para ver que onda
-def mutation_func(offspring, ga_instance):
-    # Seleccionar random un plato principal
-    # Seleccionar random un acompañamiento
-    # Seleccionar random una bebida
-    return offspring
 
-# Tambien se pueden definir padres y crossover
+    principal_s = str(principal)
+    principal_s = principal_s[1:]
+    principal_s = " ".join(char for char in principal_s)
+    acomp_s = str(acomp)
+    acomp_s = acomp_s[1:]
+    acomp_s = " ".join(char for char in acomp_s)
+    bebida_s = str(bebida)
+    bebida_s = bebida_s[1:]
+    bebida_s = " ".join(char for char in bebida_s)
+
+    principal_lista = [int(num) for num in principal_s.split()]
+    acomp_lista = [int(num) for num in acomp_s.split()]
+    bebida_lista = [int(num) for num in bebida_s.split()]
+
+    return round((caloria_calculator(principal_lista, acomp_lista, bebida_lista) + gusto_calculator(principal_lista, acomp_lista, bebida_lista)) / 2)
+
+def main():
+    num_generations = 50
+    num_parents_mating = 3
+
+    parent_selection_type = "sss"
+    keep_parents = 1
+
+    crossover_type = "single_point"
+
+    mutation_type = "random"
+    mutation_num_genes = 2
+
+    # Poblacion inicial para limitar genes
+    initial_population = [
+        [milanesa, papas_fritas, agua],
+        [ensalada, arroz, bebida_cola],
+        [bife, pan, jugo]
+    ]
+
+    gene_space = [[milanesa, ensalada, bife, pechuga_de_pollo, hamburguesa],
+                  [pure_de_papa, ensalada_side, papas_fritas, arroz, pan],
+                  [agua, bebida_cola, bebida_cola_sin_azucar, jugo, bebida_energetica]]
+
+    # Creo clase de Pygad con parametros
+    ga_instance = pg.GA(num_generations=num_generations,
+                       num_parents_mating=num_parents_mating,
+                       fitness_func=fitness_func,
+                       parent_selection_type=parent_selection_type,
+                       keep_parents=keep_parents,
+                       crossover_type=crossover_type,
+                       mutation_type= mutation_type,
+                       mutation_num_genes=mutation_num_genes,
+                       initial_population=initial_population,
+                       gene_space = gene_space)
+
+    ga_instance.run()
+
+    print(ga_instance.best_solution())
+
+if __name__=="__main__":
+    main()
--- platos.py
+++ platos.py
@@ -7,29 +7,36 @@
 # 10 Alto
 # 11 Excesivo
 
+# ARRANCO CON 9 PARA QUE NO ME TOME OCTAL, LO CORTO EN FITNESS
+
 # Plato principal
 # 7 bits
 
-milanesa         = [1,0, 0,1,1,0,1]
-ensalada         = [0,0, 0,0,0,1,0]
-bife             = [0,1, 0,1,0,1,0]
-pechuga_de_pollo = [0,0, 0,0,1,1,1]
-hamburguesa      = [1,1, 0,1,1,1,1]
+milanesa         = 91010111
+ensalada         = 90000111
+bife             = 90101010
+pechuga_de_pollo = 90001011
+hamburguesa      = 91101110
+
 
 # Acompañamiento
 # 4 bits
 
-pure_de_papa = [0,1, 1,0]
-ensalada_side     = [0,0, 0,0]
-papas_fritas = [1,1, 1,1]
-arroz        = [0,1, 0,0]
-pan          = [0,1, 0,1]
+pure_de_papa  = 90110
+ensalada_side = 90000
+papas_fritas  = 91111
+arroz         = 90100
+pan           = 90101
 
 # Bebida
 # 4 bits
 
-agua                   = [0,0, 0,1]
-bebida_cola            = [1,1, 1,1]
-bebida_cola_sin_azucar = [0,0, 1,0]
-jugo                   = [0,1, 1,0]
-bebida_energetica      = [1,1, 0,1]
+agua                   = 90001
+bebida_cola            = 91111
+bebida_cola_sin_azucar = 90010
+jugo                   = 90110
+bebida_energetica      = 91101
+
+principales = [milanesa, ensalada, bife, pechuga_de_pollo, hamburguesa]
+acomps = [pure_de_papa, ensalada_side, papas_fritas, arroz, pan]
+bebidas = [agua, bebida_cola, bebida_cola_sin_azucar, jugo, bebida_energetica]
